8a6479d953cdf568ee4c4c7b4978e5ac969fc92d
goodwinns16 <sarahgoodwin496@gmail.com>
2025-03-08 21:43:53 -0500

    Add files via upload

--- CSC 102 User Input Validation 3 7 25.js
+++ CSC 102 User Input Validation 3 7 25.js
@@ -12,7 +12,16 @@ function checkCreds()
         document.getElementById("loginStatus").innerHTML = "Secret Squirrel keep it short and squeaky- 20 characters max!";
         //if it is they will be met by this error
     }
-    
+    else if (firstName.trim().length === 0)//first name has to have at least 1 character
+    {
+        document.getElementById("loginStatus").innerHTML = "A nameless Code Name? Impossible! Even Acorns have names, Try again, friend!"
+    }
+    else if (lastName.trim().length === 0)//first name has to have at least 1 character
+    //trim is added to remove leading and trailing spaces to ensure that a name has been entered and not just spaces
+    //length checks if field is empty after trimming
+    {
+        document.getElementById("loginStatus").innerHTML = "Mysterious, are we? Even the sneakiest squirrels have names. Pick one before the nut hoard police get suspicious!"
+    }
     else if (zipCode.length !== 5) /*zipcode must be 5 digits*/
     {
         document.getElementById("loginStatus").innerHTML = "No classified squirrel operations in that sector. Enter a valid 5-digit coordinate";
